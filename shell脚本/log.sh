75ed602d7c0c45b6845e04aa28c88a860b4572cc
车凯凯 <kkche@quantangle.com.cn>
2022-12-08 10:12:03 +0800

    update

---
+++ shell脚本/log.sh
@@ -0,0 +1,28 @@
+```shell
+WORKDIR=/data/logs
+DATE=$(date +%F)
+
+MainProcess (){
+  for DIRS in $(ls $WORKDIR)
+  do
+    [ -d $WORKDIR/$DIRS ] || continue
+    cd $WORKDIR/$DIRS
+    ls *.log &> /dev/null
+    if [ $? -eq 0 ];then
+      for FILES in $(ls *.log|egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}'|grep -v "$(date +%Y-%m-%d)")
+      do
+        gzip $FILES
+      done
+
+      for FILES in $(ls *.txt|egrep '[0-9]{4}-[0-9]{2}-[0-9]{2}'|grep -v "$(date +%Y-%m-%d)")
+      do
+        gzip $FILES
+      done
+      find . -type f -mtime +30 -name "*.gz" -exec rm -f {} \;
+    fi
+  done
+}
+
+
+MainProcess
+```
